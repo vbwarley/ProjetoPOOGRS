89e340cc1f4934e9b1cfe374d8b6377e17ec87df
dougfun <douglas.henrique.ifal@gmail.com>
2014-01-13 13:18:43 -0300

    Teste

--- ProjetoPOO - GRS/src/gui/UserInterface.java
+++ ProjetoPOO - GRS/src/gui/UserInterface.java
@@ -1,7 +1,55 @@
 package gui;
 
+import negocios.Fachada;
 
-//Modificaçao
 public class UserInterface {
 	
+	Fachada fachada = new Fachada();
+
+	public static void manterRequisicao()
+	{
+			
+	}
+		
+	public static void enviarrRequisicao()
+	{
+			
+	}
+		
+	public static void consultarrRequisicao()
+	{
+			
+	}
+		
+	public static void manterUsuario()
+	{
+			
+	}
+		
+	public static void criarUsuario()
+	{
+			
+	}
+		
+	public static void consultarUsuario()
+	{
+			
+	}
+		
+	public static void excluirUsuario()
+	{
+			
+	}
+		
+	public static void atualizarUsuario()
+	{
+			
+	}
+		
+	public static void autenticacao()
+	{
+			
+	}
+		
 }
+
--- ProjetoPOO - GRS/src/negocios/Fachada.java
+++ ProjetoPOO - GRS/src/negocios/Fachada.java
@@ -1,4 +1,49 @@
+package negocios;
+
+import java.util.Calendar;
 
 public class Fachada {
 
+	public String enviarRequisicao(String descricaoRequisicao, String tipoRequisicao, int prazoParaTermino_dias) {
+		
+		return null;
+	
+	}
+	
+	public <ArrayList>String consultarRequisicoes(Calendar data) {
+		
+		return null;
+	
+	}
+	
+	public <ArrayList>String consultarRequisicoes(String tipoRequisicao) {
+		
+		return null;
+	
+	}
+	
+	public String criarUsuario(String nome, String departamento) {
+		return null;
+	}
+	
+	public <ArrayList>String consultarUsuario(String nomeUsuario) {
+		return null;
+	}
+	
+	public String excluirUsuario(int codigoUsuario) {
+		return null;
+	}
+	
+	public String atualizarUsuario(String nome, String departamento) {
+		return null;
+	}
+	
+	public String auatenticacao(String nomeUsuario, String senha) {
+		return null;
+	}
+	
+	public String mostrarDetalhesUsuario(int codigo) {
+		return null;
+	}
+	
 }
