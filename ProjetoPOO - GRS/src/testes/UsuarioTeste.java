7bdd09f8afd30018a9e8a98237c87dec0565ff89
gudines87 <lucas.a.f.b.b@gmail.com>
2014-01-28 09:12:40 -0300

    Adicionando Testes ;)

---
+++ ProjetoPOO - GRS/src/testes/UsuarioTeste.java
@@ -0,0 +1,84 @@
+package testes;
+
+
+import static junit.framework.Assert.*;
+import negocios.Administrador;
+import negocios.Cliente;
+import negocios.Fachada;
+import negocios.Requisicao;
+import negocios.Usuario;
+
+import org.junit.Test;
+
+public class UsuarioTeste {
+
+	Usuario a = new Administrador();
+	Cliente c = new Cliente();
+	
+	@Test
+	public void testSalvarAdministrador() {
+		a.setNome("Lucas");
+		a.setSenha("Lindo!");
+		a.salvarDados();
+		assertNotNull(a.getCodigo());
+	}
+
+	@Test
+	public void testEnviarRequisicao() {
+		Requisicao r = new Requisicao();
+		r.setDescricao("Teste");
+		a.enviarRequisicao(r);
+		assertNotNull(r.getCodigo());
+	}
+	
+	@Test
+	public void testSalvarCliente(){
+		c.setNome("Warley");
+		c.setSenha("Afeminado");
+		c.salvarDados();
+		assertNotNull(c.getCodigo());
+	}
+	
+	@Test
+	public void testExcluirUsuario(){
+		c.setNome("Warley");
+		c.setSenha("Afeminado");
+		c.salvarDados();
+		assertNotNull(c.getCodigo());
+		Fachada.getInstance().excluirUsuario(c.getCodigo());
+		assertNull(c);
+	}
+	
+	@Test
+	public void testAtualizarUsuario(){
+		c.setNome("Douglas");
+		c.setSenha("corDeRosa");
+		c.salvarDados();
+		assertNotNull(c.getCodigo());
+		Usuario c1 = new Cliente();
+		c1 = c;
+		Fachada.getInstance().atualizarUsuario(c.getCodigo(), "Dodo", "Algum");
+		assertEquals(c, c1);
+	}
+	
+	@Test
+	public void testAutenticarUsuario(){
+		a.setNome("Lukinhas");
+		a.setSenha("Alguma");
+		a.salvarDados();
+		assertNotNull(a.getCodigo());
+		int codigo = Fachada.getInstance().autenticacao(a.getNome(), a.getSenha());
+		assertEquals(a.getCodigo(), codigo);
+	}
+	
+	@Test
+	public void testConsultarUsuario(){
+		a.setNome("Lukinhas");
+		a.setSenha("Alguma");
+		a.salvarDados();
+		assertNotNull(a.getCodigo());
+		String b = a.toString();
+		assertEquals(b,Fachada.getInstance().consultarUsuario(a.getCodigo()));
+	}
+
+}
